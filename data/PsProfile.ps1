f40773f2f170058ad796202fd523ff3be70abb8a
Liu Shi <liushi@microsoft.com>
2019-07-16 20:11:25 +0800

    Remove the dependency to basic.

--- data/PsProfile.ps1
+++ data/PsProfile.ps1
@@ -1,3 +1,5 @@
 # References:
 # 1. Below are the list of predefined vars that can be used:
 #    - $PSScriptRoot		[System defined] The folder path for current scipt file, NOT the caller script to call this function
+
+Import-Module -Name autoprofile -WarningAction Ignore
