2620bcf09eb70d43f160ff42b628fc638b23315b
Liu Shi <liushi@microsoft.com>
2019-07-17 13:30:43 +0800

    Update to version 10

--- data/startup/PowerShell/RegisterGlobalVars.ps1
+++
@@ -1,3 +0,0 @@
-try { Set-EnvVar -Name "SynologyDriveRoot" -Value $(Get-SynologyDriveRoot) -Target User } catch {}
-try { Set-EnvVar -Name "OneDriveRoot" -Value $(Get-OneDriveRoot) -Target User } catch {}
-try { Set-EnvVar -Name "OneDriveMicrosoftRoot" -Value $(Get-OneDriveRoot Microsoft) -Target User } catch {}
